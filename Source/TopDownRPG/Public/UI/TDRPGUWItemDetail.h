dc72d670c6d48c22f09a452367b36e463e59eacc
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-06-09 22:08:18 +0900

    Feat: 아이템 상세 UI 추가

---
+++ Source/TopDownRPG/Public/UI/TDRPGUWItemDetail.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/TDRPGUserWidget.h"
+#include "TDRPGUWItemDetail.generated.h"
+
+class UTextBlock;
+class UImage;
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNRPG_API UTDRPGUWItemDetail : public UTDRPGUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UTextBlock> NameLabel;
+	
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UTextBlock> QuantityLabel;
+	
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UTextBlock> RarityLabel;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UImage> IconImage;
+};
